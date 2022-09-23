67484836aa72a141b211f42f672768b778a02c47
jangoflyte <miguelpcastro95@gmail.com>
2022-09-23 08:33:42 -0600

    starting

---
+++ minesweeper/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Board } from './components/Board';
+
+export function App() {
+  return (
+    <div>
+      <h1>Minesweeper</h1>
+      <Board/>
+    </div>
+  );
+}
+
+
